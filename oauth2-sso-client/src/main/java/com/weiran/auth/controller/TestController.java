33bbed6515d4ade00ac5366a90353184ef663fde
weiran1999 <907221539@qq.com>
2021-09-28 17:18:12 +0800

    尚未优化和集成网关

--- oauth2-sso-client/src/main/java/com/weiran/auth/controller/TestController.java
+++ oauth2-sso-client/src/main/java/com/weiran/auth/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.weiran.auth.controller;
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,13 @@ public class TestController {
     @GetMapping("/getCurrentTest")
     public Object getCurrentTest(Authentication authentication) {
         return authentication;
+    }
+
+    // 需要admin权限的接口
+    @PreAuthorize("hasAuthority('admin')")
+    @GetMapping("/ifAdmin")
+    public Object adminAuth() {
+        return "Has admin auth!";
     }
 
 }
