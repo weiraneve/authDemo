ce8b36f840af401950fec52b1d6b6e9423b866b2
weiran1999 <907221539@qq.com>
2021-09-28 11:34:41 +0800

    增加JWT

--- oauth2-JWT/src/main/java/com/weiran/auth/config/AuthorizationServerConfig.java
+++ oauth2-JWT/src/main/java/com/weiran/auth/config/AuthorizationServerConfig.java
@@ -1,22 +1,27 @@
 package com.weiran.auth.config;
 
+import com.weiran.auth.jwt.JwtTokenEnhancer;
 import com.weiran.auth.service.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 认证服务器配置
+ * Created by macro on 2019/9/30.
  */
 @Configuration
 @EnableAuthorizationServer
@@ -31,28 +36,35 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private RedisConnectionFactory connectionFactory;
+    // 指定令牌存储策略为普通Redis的token
+//    @Autowired
+//    @Qualifier("redisTokenStore")
+//    private TokenStore tokenStore;
 
-    /**
-     *  oauth2 令牌存储在redis
-     **/
-    @Bean
-    public TokenStore tokenStore() {
-        RedisTokenStore redis = new RedisTokenStore(connectionFactory);
-        return redis;
-    }
+    @Autowired
+    @Qualifier("jwtTokenStore")
+    private TokenStore tokenStore;
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+    @Autowired
+    private JwtTokenEnhancer jwtTokenEnhancer;
 
     /**
      * 使用密码模式需要配置
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        List<TokenEnhancer> delegates = new ArrayList<>();
+        delegates.add(jwtTokenEnhancer); // 配置JWT的内容增强器
+        delegates.add(jwtAccessTokenConverter);
+        enhancerChain.setTokenEnhancers(delegates);
         endpoints.authenticationManager(authenticationManager)
-                .tokenStore(tokenStore())
-                .userDetailsService(userService);
+                .userDetailsService(userService)
+                .tokenStore(tokenStore) // 配置令牌存储策略
+                .accessTokenConverter(jwtAccessTokenConverter)
+                .tokenEnhancer(enhancerChain);
     }
-
 
     /**
      *  用来配置客户端详情服务
